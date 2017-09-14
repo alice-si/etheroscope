316b43eb2e74995b37c5db3131e27195f3d5f998
jakub-wojciechowski <jakub@alice.si>
2017-09-14 13:51:46 +0200

    History cache

--- devServer/controller/contract.js
+++ devServer/controller/contract.js
@@ -1,6 +1,9 @@
 var Fs = require('fs');
 var Parity = require("../service/parity");
 var Promise = require('bluebird');
+var Level = require('level');
+
+var historyCache = Level('./db/history');
 
 module.exports = function (app) {
 
@@ -16,31 +19,39 @@ module.exports = function (app) {
     });
 
     app.get('/api/getHistory/:contractAddress/:method', function (req, res) {
-        return Parity.getContract(req.params.contractAddress).then(function (contract) {
-            return Parity.getHistory(req.params.contractAddress).then(function (events) {
-                var history = [];
-                var i=0;
-                var prevTime = 0;
-                Promise.map(events, function (event) {
-                    return new Promise(function (resolve) {
-                        Parity.getBlockTime(event.blockNumber.valueOf()).then(function (time) {
-                            if (time === prevTime) return resolve();
-                            prevTime = time;
-                            Parity.queryAtBlock(contract[req.params.method], event.blockNumber.valueOf()).then(function (val) {
-                                history.push([time, val]);
-                                console.log("Fetched: " + i + " time: " + time + " val: " + val);
-                                i++;
-                                return resolve(val);
+        var key = req.params.contractAddress + "/" + req.params.method;
+        historyCache.get(key, function (err, value) {
+                if (err) {
+                    return Parity.getContract(req.params.contractAddress).then(function (contract) {
+                        return Parity.getHistory(req.params.contractAddress).then(function (events) {
+                            var history = [];
+                            var i = 0;
+                            var prevTime = 0;
+                            Promise.map(events, function (event) {
+                                return new Promise(function (resolve) {
+                                    Parity.getBlockTime(event.blockNumber.valueOf()).then(function (time) {
+                                        if (time === prevTime) return resolve();
+                                        prevTime = time;
+                                        Parity.queryAtBlock(contract[req.params.method], event.blockNumber.valueOf()).then(function (val) {
+                                            history.push([time, val]);
+                                            console.log("Fetched: " + i + " time: " + time + " val: " + val);
+                                            i++;
+                                            return resolve(val);
+                                        });
+                                    });
+                                });
+                            }, {concurrency: 20}).then(function () {
+                                history.sort(function (a, b) {
+                                    return a[0] - b[0];
+                                });
+                                historyCache.put(key, JSON.stringify(history));
+                                return res.status(200).json(history);
                             });
                         });
                     });
-                }, {concurrency: 20}).then(function () {
-                   history.sort(function (a, b) {
-                      return a[0] - b[0];
-                   });
-                   return res.status(200).json(history);
-                });
-            });
-        });
-    })
-};
+                } else {
+                    return res.status(200).json(JSON.parse(value));
+                }
+            })
+        })
+}
