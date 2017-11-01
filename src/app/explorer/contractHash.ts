3c733e33855372b45f128f7eb92d04f1d1668a41
g-suraj <sg6215@ic.ac.uk>
2017-11-01 14:55:40 +0000

    Merge fixes, lint fixes and basic testing

--- src/app/explorer/contractHash.ts
+++ src/app/explorer/contractHash.ts
@@ -1,3 +1,3 @@
-export var contractHash = [{"name": "Alice.si", "hash": "0xBd897c8885b40d014Fb7941B3043B21adcC9ca1C"},
-							  {"name": "The DAO", "hash": "0xbb9bc244d798123fde783fcc1c72d3bb8c189413"},
-							  {"name": "DigixCrowdSale", "hash": "0xf0160428a8552ac9bb7e050d90eeade4ddd52843"}];
+export let contractHash = [{"name": "Alice.si", "hash": "0xBd897c8885b40d014Fb7941B3043B21adcC9ca1C"},
+  {"name": "The DAO", "hash": "0xbb9bc244d798123fde783fcc1c72d3bb8c189413"},
+  {"name": "DigixCrowdSale", "hash": "0xf0160428a8552ac9bb7e050d90eeade4ddd52843"}];
