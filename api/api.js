1efc7c819a7fdf520706411e9cd1ca20873b5037
Will Woodacre <wjw15@ic.ac.uk>
2017-11-03 15:11:17 +0000

    Initial attempt at using rooms rather than individual sockets to give datapoints back to front end

--- api/api.js
+++ api/api.js
@@ -49,7 +49,7 @@ module.exports = function (app, db, io) {
       })
   })
 
-  function sendDataPointsFromParity (socket, contractAddress, method, from, to,
+  function sendDataPointsFromParity (contractAddress, method, from, to,
     totalFrom, totalTo) {
     console.log('Sending history from parity')
     // First we obtain the contract.
@@ -69,19 +69,19 @@ module.exports = function (app, db, io) {
         })
         .then(function (results) {
           console.log('generated data points successfully')
-          socket.emit('getHistoryResponse', { error: false, contract: contractAddress, method: method, from: from, to: to, results: results })
+          io.sockets.in(contractAddress+method).emit('getHistoryResponse', { error: false, contract: contractAddress, method: method, from: from, to: to, results: results })
           return resolve()
         })
         .catch(function (err) {
           console.log('Error in parity sending')
           console.log(err)
-          socket.emit('getHistoryResponse', { error: true })
+          io.sockets.in(contractAddress+method).emit('getHistoryResponse', { error: true })
           return reject(err)
         })
     })
   }
 
-  function sendAllDataPointsFromDB (socket, address, method) {
+  function sendAllDataPointsFromDB (address, method) {
     console.log('Sending history from db')
     db.getDataPoints(address.substr(2), method)
       .then((dataPoints) => {
@@ -91,18 +91,19 @@ module.exports = function (app, db, io) {
       })
       .then((dataPoints) => {
         console.dir(dataPoints)
-        socket.emit('getHistoryResponse', { error: false, contract: address, method: method, results: dataPoints })
+        io.sockets.in(address+method).emit('getHistoryResponse', { error: false, contract: address, method: method, results: dataPoints })
       })
       .catch(function (err) {
         console.log('Error sending datapoints from DD')
         console.log(err)
-        socket.emit('getHistoryResponse', { error: true })
+        io.sockets.in(address+method).emit('getHistoryResponse', { error: true })
       })
   }
 
   io.on('connection', function (socket) {
     socket.on('getHistory', ([address, method]) => {
-      sendHistory(socket, address, method)
+      socket.join(address+method)
+      sendHistory(address, method)
     })
   })
 
@@ -110,9 +111,9 @@ module.exports = function (app, db, io) {
     console.log('User has disconnected')
   })
 
-  function sendHistory (socket, address, method) {
+  function sendHistory (address, method) {
     // Send every point we have in the db so far
-    sendAllDataPointsFromDB(socket, address, method)
+    sendAllDataPointsFromDB(address, method)
 
     // If there is already a caching process, we don't need to set one up
     if (methodCachesInProgress.has(address + method)) {
@@ -132,7 +133,7 @@ module.exports = function (app, db, io) {
           from = latestBlock
           to = latestBlock
         }
-        cacheMorePoints(socket, address, method, from, to, latestBlock)
+        cacheMorePoints(address, method, from, to, latestBlock)
       })
     })
     .catch((err) => {
@@ -141,7 +142,7 @@ module.exports = function (app, db, io) {
   }
 
   // from, to and latestBlock are exclusive
-  function cacheMorePoints (socket, address, method, from, to, latestBlock) {
+  function cacheMorePoints (address, method, from, to, latestBlock) {
     const chunkSize = 1000
     if (to === latestBlock) {
       if (from === 1) {
@@ -149,15 +150,15 @@ module.exports = function (app, db, io) {
         return
       }
       let newFrom = Math.max(from - chunkSize, 1)
-      sendDataPointsFromParity(socket, address, method, newFrom, from - 1, newFrom, to)
+      sendDataPointsFromParity(address, method, newFrom, from - 1, newFrom, to)
       .then(() => {
-        cacheMorePoints(socket, address, method, newFrom, to, latestBlock)
+        cacheMorePoints(address, method, newFrom, to, latestBlock)
       })
     } else {
       let newTo = Math.min(to + chunkSize, latestBlock)
-      sendDataPointsFromParity(socket, address, method, to + 1, newTo, from, newTo)
+      sendDataPointsFromParity(address, method, to + 1, newTo, from, newTo)
       .then(() => {
-        cacheMorePoints(socket, address, method, from, newTo, latestBlock)
+        cacheMorePoints(address, method, from, newTo, latestBlock)
       })
     }
   }
