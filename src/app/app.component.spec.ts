fa2196526401a4f8869b7c3d945c1d2984d4d840
Josh Cooper <jsc115@ic.ac.uk>
2017-11-09 14:52:50 +0000

    Have refactored the project so that it now no longer includes the unecessary about component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,7 +3,7 @@
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { HomeComponent } from "./home/home.component";
-import { AboutComponent } from "./about/about.component";
+import { ExplorerComponent } from "./explorer/explorer.component";
 import { ClarityModule } from "clarity-angular";
 import { ROUTING } from "./app.routing";
 import { APP_BASE_HREF } from "@angular/common";
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
         TestBed.configureTestingModule({
             declarations: [
                 AppComponent,
-                AboutComponent,
+                ExplorerComponent,
                 HomeComponent
             ],
             imports: [
