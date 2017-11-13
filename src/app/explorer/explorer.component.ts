9d7d6f2ebb793dfd003838814610177e57641022
g-suraj <sg6215@ic.ac.uk>
2017-11-13 23:40:10 +0000

    Current variable added

--- src/app/explorer/explorer.component.ts
+++ src/app/explorer/explorer.component.ts
@@ -8,41 +8,6 @@ import { ContractService } from "../_services/contract.service";
 })
 
 export class ExplorerComponent {
-  /*
-  public items: Array<string> = ["Alice.si", "0xBd897c8885b40d014Fb7941B3043B21adcC9ca1C",
-  "The DAO", "0xbb9bc244d798123fde783fcc1c72d3bb8c189413",
-  "DigixCrowdSale", "0xf0160428a8552ac9bb7e050d90eeade4ddd52843"];
-
-  private value: any = {};
-  private _disabledV: string = '0';
-  public disabled: boolean = false;
-
-  private get disabledV(): string {
-    return this._disabledV;
-  }
-
-  private set disabledV(value: string) {
-    this._disabledV = value;
-    this.disabled = this._disabledV === '1';
-  }
-
-  public selected(value: any): void {
-    console.log('Selected value is: ', value);
-  }
-
-  public removed(value: any): void {
-    console.log('Removed value is: ', value);
-  }
-
-  public typed(value: any): void {
-    console.log('New search input: ', value);
-  }
-
-  public refreshValue(value: any): void {
-    this.value = value;
-  }
-  */
-
   single: any[];
   multi: any[];
 
@@ -58,6 +23,7 @@ export class ExplorerComponent {
   methodDatapoints: number[][];
   lastMethod: string;
   lastContract: string;
+  placeholder: string;
 
   selectedCompany: any;
 
@@ -85,6 +51,7 @@ export class ExplorerComponent {
 
   constructor(private contractService: ContractService) {
     this.curContractID = '';
+    this.placeholder = null;
     this.single = [];
     this.multi = [];
     this.displayMethods = false;
@@ -146,6 +113,7 @@ export class ExplorerComponent {
         console.log(error);
       },
       () => {
+        this.placeholder = contract;
         console.log("completed contract exploring");
         this.displayMethods = true;
       }
