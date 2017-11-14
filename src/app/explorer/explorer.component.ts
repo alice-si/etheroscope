6ac89d145eb23440c7e05424ec3ec7cb8e6ed653
GitLab Runner <gitlab-runner@Etheroscope-Instance.l0cryh30ub1e5gp4mjhrzf5vpa.zx.internal.cloudapp.net>
2017-11-14 14:05:06 +0000

    Added initial framework for filters on datapoints

--- src/app/explorer/explorer.component.ts
+++ src/app/explorer/explorer.component.ts
@@ -58,6 +58,7 @@ export class ExplorerComponent {
   methodDatapoints: number[][];
   lastMethod: string;
   lastContract: string;
+  datapointFilters: {message: string, filter: ((datapoint: any[]) => boolean)}[];
 
   selectedCompany: any;
 
@@ -95,28 +96,16 @@ export class ExplorerComponent {
     this.lastContract = null;
     this.graphDatapoints = [];
     this.methodDatapoints = [];
+    this.datapointFilters = [];
     this.contractService.getHistoryEvent().subscribe(
       (datapoints: any) => {
         console.log("updating...")
         this.graphDatapoints = [];
         if (datapoints.results.length !== 0) {
           this.methodDatapoints = this.methodDatapoints.concat(datapoints.results);
-          // get rid of datapoints with duplicate times
-          let seenTime = {};
-          this.methodDatapoints.filter( (point) => {
-            if (seenTime.hasOwnProperty(point[0])) {
-              return false;
-            }
-            seenTime[point[0]] = true;
-            return true;
-          });
-          console.log(this.methodDatapoints.length + " method datapoints");
-          let samples = 100;
-          let intervals = Math.floor(this.methodDatapoints.length / samples);
-          for (let i = 0; i < samples; i++) {
-            this.graphDatapoints[i] = this.methodDatapoints[i * intervals];
-          }
-          console.log(this.graphDatapoints.length + " graph datapoints");
+          this.removeDuplicateDatapoints();
+          this.filterGraphDatapoints();
+
           console.log("Updating graph");
           this.updateGraph();
         }
@@ -133,6 +122,40 @@ export class ExplorerComponent {
 
   onSelect(event) {
     console.log(event);
+  }
+
+  removeDuplicateDatapoints() {
+    // get rid of datapoints with duplicate times
+    let seenTime = {};
+    this.methodDatapoints.filter( (point) => {
+      if (seenTime.hasOwnProperty(point[0])) {
+        return false;
+      }
+      seenTime[point[0]] = true;
+      return true;
+    });
+  }
+
+  filterGraphDatapoints() {
+    this.graphDatapoints = this.methodDatapoints;
+    this.graphDatapoints.filter( (point) => {
+      let len = this.datapointFilters.length;
+      for (let i = 0; i < len; i++) {
+        if (!this.datapointFilters[i].filter(point)) {
+          return false;
+        }
+      }
+      return true;
+    })
+  }
+
+  filterOnDatesBetween(startDate: number, endDate: number, message: string) {
+    this.datapointFilters.push({
+      message: message,
+      filter: (point) => {
+        return point[0] >= startDate && point[0] <= endDate;
+      }
+    })
   }
 
   exploreContract(contract: string) {
