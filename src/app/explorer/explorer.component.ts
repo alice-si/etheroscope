82b2a44770b693e3100ea27b0b99fbd7737cf9f6
Finn Johnson <finn@Etheroscope-Instance.l0cryh30ub1e5gp4mjhrzf5vpa.zx.internal.cloudapp.net>
2017-11-15 20:14:45 +0000

    Removed un-needed array of contracts

--- src/app/explorer/explorer.component.ts
+++ src/app/explorer/explorer.component.ts
@@ -20,10 +20,6 @@ export class ExplorerComponent {
   single: any[];
   multi: any[];
 
-  contractHash: any[] = [{"name": "Alice.si", "hash": "0xBd897c8885b40d014Fb7941B3043B21adcC9ca1C"},
-    {"name": "The DAO", "hash": "0xbb9bc244d798123fde783fcc1c72d3bb8c189413"},
-    {"name": "DigixCrowdSale", "hash": "0xf0160428a8552ac9bb7e050d90eeade4ddd52843"}];
-
   curContractID: string;
   curContractName: string;
   methods: string[];
