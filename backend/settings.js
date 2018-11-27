1aaa637170087cb6577a5ba97282f4e6f1aacb46
unknown <adampoczta7@gmail.com>
2018-11-27 12:09:14 +0100

    yolo 2

--- backend/settings.js
+++ backend/settings.js
@@ -1,13 +1,13 @@
 module.exports.fullBlockchainPath = '../geth-blockchains/fullRinkebyBlockchain/geth/chaindata'
 // module.exports.fullBlockchainPath = '../../Alice/dirForFullRinkeby/geth/chaindata'
 
-module.exports.gethHost = 'alice-eth-geth-service'
+module.exports.gethHost = '10.3.255.188'
 
 module.exports.mysqlConnectionOptions = {
   connectionLimit: 10,
   connectionTimeout: 10000,
   // host: '192.168.99.100',
-  host: 'localhost',
+  host: '10.3.240.97',
   port: '8083',
   user: 'root',
   password: 'wp',
