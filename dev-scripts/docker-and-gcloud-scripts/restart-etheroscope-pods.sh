4dfbc5bdd5deabcd8f4098a02f229aba07b40126
unknown <adampoczta7@gmail.com>
2018-11-28 22:05:15 +0100

    halo

--- dev-scripts/docker-and-gcloud-scripts/restart-etheroscope-pods.sh
+++ dev-scripts/docker-and-gcloud-scripts/restart-etheroscope-pods.sh
@@ -1,5 +1,5 @@
 # only pods with "etheroscope" phrase in name
-echo "only pods with "etheroscope" phrase in name"
+echo "deleting only pods with \"etheroscope\" phrase in name"
 pods=$(kubectl get pods | grep etheroscope | awk '{print $1;}' )
 for pod in $pods
 do
