533a1e88eac75fcc6600967ff64397cafdc9b061
olearczuk <olearczuk.szymon@gmail.com>
2018-11-28 20:37:50 +0100

    correcting endpoints for backend in environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
     // socketURL: 'http://etheroscope.alice.si:8081/'
     // apiURL: 'localhost:8080/',
     // socketURL: 'localhost:8081/'
-    apiURL: '10.3.240.97:8080/',
-    socketURL: '10.3.240.97:8081/',
+    apiURL: 'http://10.3.240.97:8080/',
+    socketURL: 'http://10.3.240.97:8081/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,6 +9,6 @@ export const environment = {
     // socketURL: 'http://etheroscope.alice.si:8081/'
     // apiURL: 'localhost:8080/',
     // socketURL: 'localhost:8081/'
-    apiURL: '10.3.240.97:8080/',
-    socketURL: '10.3.240.97:8081/',
+    apiURL: 'http://10.3.240.97:8080/',
+    socketURL: 'htpp://10.3.240.97:8081/',
 };
