61a3232d35840adacdbd4a947755c635ded39ebd
unknown <adampoczta7@gmail.com>
2018-11-29 17:13:14 +0100

    some new logs in server.js

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
     // socketURL: 'http://etheroscope.alice.si:8081/'
     // apiURL: 'localhost:8080/',
     // socketURL: 'localhost:8081/'
-    apiURL: 'http://10.3.241.13:8080/',
-    socketURL: 'htpp://10.3.241.13:8081/',
+    apiURL: 'http://35.197.241.242:8080/',
+    socketURL: 'http://35.230.140.68:8081/',
 };
