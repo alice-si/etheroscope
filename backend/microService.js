9d9e30f789b9b7e88b245a6338e85f4b29cbef37
olearczuk <olearczuk.szymon@gmail.com>
2018-11-30 20:27:12 +0100

    fixing CORS error related to microService - added Access-Control-Allow-Headers

--- backend/microService.js
+++ backend/microService.js
@@ -31,8 +31,9 @@ db.poolConnect().then(() => {
   app.use(morgan('dev'))
 
   app.use(function (req, res, next) {
-    console.log('Access-Control-Allow-Origin');
+    console.log('Access-Control-Allow-Origin')
     res.setHeader('Access-Control-Allow-Origin', req.header('origin'))
+    res.setHeader('Access-Control-Allow-Headers', 'Origin')
     next()
   })
 
