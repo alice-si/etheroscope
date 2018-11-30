2ed0dcbd9c2920cb83a5b7cd72b51ef98137b3ea
brzezinskimarcin <brzezinskimarcin97@gmail.com>
2018-11-30 21:40:46 +0100

    cors

--- backend/microService.js
+++ backend/microService.js
@@ -19,8 +19,7 @@ let express = require('express')
 let app = express()
 let server = require('http').createServer(app)
 let io = require('socket.io')(server, {
-  origins: 'http://35.242.161.116',
-  transports: ['websocket']
+  origins: 'http://35.242.161.116'
 })
   // .use(cors({origin: 'http://35.242.161.116', credentials: true}))
 // .set('origins', 'http://35.242.161.116:80')
