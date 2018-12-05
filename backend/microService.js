f1d76c016d8900e36a280d796d6cb0919e20814b
olearczuk <olearczuk.szymon@gmail.com>
2018-12-05 16:04:17 +0100

    let io = require('socket.io')(server, {origins: '*35.242.161.116*'})

--- backend/microService.js
+++ backend/microService.js
@@ -22,7 +22,7 @@ app.use(cors({
   origin: 'http://35.242.161.116'
 }))
 let server = require('http').createServer(app)
-let io = require('socket.io')(server, {origins: '*http://35.242.161.116*'})
+let io = require('socket.io')(server, {origins: '*35.242.161.116*'})
 
 // (server, {
 //   origins: 'http://35.246.65.214:8081/*'
