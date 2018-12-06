f36ca6644d366c4e1dded0ae3f60f83c45a1b7a3
olearczuk <olearczuk.szymon@gmail.com>
2018-12-06 14:23:07 +0100

    some lost files

---
+++ backend/settings.js
@@ -0,0 +1,15 @@
+module.exports.fullBlockchainPath = '/usr/src/app/etheroscope/geth-blockchains/fullRinkebyBlockchain/geth/chaindata'
+// module.exports.fullBlockchainPath = '../../Alice/dirForFullRinkeby/geth/chaindata'
+
+module.exports.gethHost = '10.3.255.188'
+
+module.exports.mysqlConnectionOptions = {
+  connectionLimit: 10,
+  connectionTimeout: 10000,
+  // host: '192.168.99.100',
+  host: '10.3.240.97',
+  port: '8083',
+  user: 'root',
+  password: 'wp',
+  database: 'etheroscope'
+}
