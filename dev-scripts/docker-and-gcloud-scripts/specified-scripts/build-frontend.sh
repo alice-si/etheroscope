2996f8793a4f8bfda1e49cd21d9d552f04945ce7
olearczuk <olearczuk.szymon@gmail.com>
2018-12-08 21:36:55 +0100

    modified docker and gcloud scripts because of new project structure

--- dev-scripts/docker-and-gcloud-scripts/specified-scripts/build-frontend.sh
+++
@@ -1,4 +0,0 @@
-docker build -t etheroscope-frontend ../../deploy-scripts/dockerfiles/etheroscope-frontend
-docker tag etheroscope-frontend gcr.io/etheroscope/etheroscope-frontend
-docker push gcr.io/etheroscope/etheroscope-frontend
-
--- dev-scripts/docker-and-gcloud-scripts/specified-scripts/build-serwer.sh
+++
@@ -1,4 +0,0 @@
-docker build -t etheroscope-serwer ../../deploy-scripts/dockerfiles/etheroscope-serwer
-docker tag etheroscope-serwer gcr.io/etheroscope/etheroscope-serwer
-docker push gcr.io/etheroscope/etheroscope-serwer
-
