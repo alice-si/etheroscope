ba1cc46fb31e00b736b5bb72928e5922f319d2ca
olearczuk <olearczuk.szymon@gmail.com>
2018-12-08 23:38:52 +0100

    final changes

--- dev-scripts/docker-and-gcloud-scripts/build-by-etheroscope-name.sh
+++ dev-scripts/docker-and-gcloud-scripts/build-by-etheroscope-name.sh
@@ -1,6 +1,11 @@
 # Will build docker file with name etheroscope-$1
 echo "Will build and push to gcr.io repo docker with name: etheroscope-"first comandline argument" (current: etheroscope-$1)"
-docker build -t etheroscope-$1 ../../backend/$1
+if [ $1 = "frontend" ]
+then
+    docker build -t etheroscope-$1 ../../$1
+else
+    docker build -t etheroscope-$1 ../../backend/$1
+fi
 docker tag etheroscope-$1 gcr.io/etheroscope/etheroscope-$1
 docker push gcr.io/etheroscope/etheroscope-$1
 
