698235fe86d67306d88c991a43d4f5e88ffb4517
olearczuk <olearczuk.szymon@gmail.com>
2018-12-30 20:49:28 +0100

    set environment variables on linux + edited runAllLocalLinux.sh

--- dev-scripts/local-work/runAllLocalLinux.sh
+++ dev-scripts/local-work/runAllLocalLinux.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+if [[ "$#" -ne 1 ]]; then
+    echo "usage: ./runLocalLinux.sh <geth directory>"
+    exit 1
+fi
+
+export ETHEROSCOPEGETHHOST="localhost:8545"
+export ETHEROSCOPEMARIADB="localhost:8083"
+export ETHEROSCOPEMICROSERVICE="localhost:8081"
+export ETHEROSCOPESERVER="localhost:8082"
+export ETHEROSCOPEFRONTEND="localhost:1234"
+export ETHEROSCOPEBLOCKCHAIN=$1
+export ETHEROSCOPEPARITYMAINNET="35.230.140.68:8545"
+
 docker-compose -f ../../dependencies/mariadb/docker-compose.yml up -d &\
 
 cd ../../frontend &&\
