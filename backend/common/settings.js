394573ac3341c7d367cda58fe99048c3df76e24d
olearczuk <olearczuk.szymon@gmail.com>
2019-04-12 22:44:06 +0200

    cleared yamls in backend and corrected small mistake with port value

--- backend/common/settings.js
+++ backend/common/settings.js
@@ -2,7 +2,7 @@ module.exports.ETHEROSCOPEPARITYMAINNET="35.246.67.158:8545"
 
 module.exports.server = {
   etherscanAPIKey: "RVDWXC49N3E3RHS6BX77Y24F6DFA8YTK23",
-  port: 8082,
+  port: 8080,
 }
 
 module.exports.dataPointsService = {
