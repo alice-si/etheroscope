d3ada3129c00bd5eadcdf405f1ca3d8f9bccf2e8
olearczuk <olearczuk.szymon@gmail.com>
2019-05-14 13:55:43 +0200

    change length of transactionHash

--- backend/db/models/transaction.js
+++ backend/db/models/transaction.js
@@ -4,7 +4,7 @@ module.exports = (sequelize, DataTypes) => {
     const Transaction = sequelize.define('Transaction', {
             // if value is null then this record was put into database in order to mark actual cached range
             value: { type: DataTypes.BIGINT(80), defaultValue: null },
-            transactionHash: { type: DataTypes.STRING(40), defaultValue: null },
+            transactionHash: { type: DataTypes.STRING(66), defaultValue: null },
             from: { type: DataTypes.STRING(42), allowNull: false },
             to: { type: DataTypes.STRING(42), allowNull: false },
         },
