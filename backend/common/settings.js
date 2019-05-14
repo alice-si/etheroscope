29b7d6190a454dac919279f38999a506d72c2adf
olearczuk <olearczuk.szymon@gmail.com>
2019-05-14 19:38:57 +0200

    General server refactor, changes made: 1. api.js     - '/api/search' -> changed from post to get     - `/api/explore` -> returns status code 200 with null in body, when address is not verified 2. parity.js     - getContract and getContractInfoFromEtherscan now return null, when contract is not verified 3. server.js     - changed Access-Control-Allow-Origin, so now server accepts requests only from frontend 4. settings.js     - changes related to changes in parity and server 5. serverDeployment.yaml     - change related to change in server 6. frontend files     - changes related to changes in '/api/search' in api

--- backend/common/settings.js
+++ backend/common/settings.js
@@ -1,9 +1,13 @@
 module.exports.ETHEROSCOPEPARITYMAINNET="35.246.67.158:8545"
+module.exports.allowedOrigin = process.env.FRONTEND_IP || "*"
 
 module.exports.server = {
   etherscanAPIKey: "RVDWXC49N3E3RHS6BX77Y24F6DFA8YTK23",
   port: 8080,
   cacheChunkSize: 100000,
+  popularContractsDays: 7,
+  popularContractsLimit: 10,
+  contractNotVerified: 'Contract source code not verified',
 }
 
 module.exports.dataPointsService = {
